1b1c9fb0c66f0ba6ae96d373fcb40e80fc3442d0
adnanalemran <adnanalemranontor@gmail.com>
2025-01-15 23:07:22 -0800

    Refactor Footer component to use Chakra UI Box for styling

--- reactContext/src/components/Shared/Footer.tsx
+++ reactContext/src/components/Shared/Footer.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { Box } from "@chakra-ui/react";
 
 const Footer = () => {
-  return <div>this an footer</div>;
+  return (
+    <Box bg="tomato" w="100%" p={4} color="white">
+      This is the Box
+    </Box>
+  );
 };
 
 export default Footer;
